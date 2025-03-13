169908700fa99558e5b19806274705c817783d08
Nick Steele <nsteele@users.noreply.github.com>
2025-03-13 15:41:43 +0000

    Re-organize nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,109 +1,3 @@
--- Auto install of lazyvim --
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none", "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git", lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-      spec = {
-
-        -- Add your plugins here --
-        --
-        -- { "folke/which-key.nvim" },
-	 {"nvim-lua/plenary.nvim"},
-	 {"christoomey/vim-tmux-navigator"},
-         { 
-           "nvim-treesitter/nvim-treesitter",
-	   build = ":TSUpdate",
-	   config = function()
-             require("nvim-treesitter.configs").setup({
-               highlight = {
-                 enable = true,
-               },
-               ensure_installed = {
-                 "python",
-                 "cpp",
-                 -- Add other languages here
-               },
-               auto_install = true,
-             })
-             end,
-         },
-         {
-           "nvim-telescope/telescope.nvim",
-           cmd = "Telescope",
-           dependencies = {
-             "nvim-lua/plenary.nvim",
-           },
-           config = function()
-             require("telescope").setup({
-               defaults = {
-                 mappings = {
-                   i = {
-                     ["<C-k>"] = "move_selection_previous",
-                     ["<C-j>"] = "move_selection_next",
-                   },
-                 },
-               },
-             })
-           end,
-         },
-	 { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
-	 {
-           "folke/tokyonight.nvim",
-           priority = 1000, -- make sure to load this before all the other start plugins
-           config = function()
-             local bg = "#011628"
-             local bg_dark = "#011423"
-             local bg_highlight = "#143652"
-             local bg_search = "#0A64AC"
-             local bg_visual = "#275378"
-             local fg = "#CBE0F0"
-             local fg_dark = "#B4D0E9"
-             local fg_gutter = "#627E97"
-             local border = "#547998"
-
-             require("tokyonight").setup({
-               style = "night",
-               on_colors = function(colors)
-                 colors.bg = bg
-                 colors.bg_dark = bg_dark
-                 colors.bg_float = bg_dark
-                 colors.bg_highlight = bg_highlight
-                 colors.bg_popup = bg_dark
-                 colors.bg_search = bg_search
-                 colors.bg_sidebar = bg_dark
-                 colors.bg_statusline = bg_dark
-                 colors.bg_visual = bg_visual
-                 colors.border = border
-                 colors.fg = fg
-                 colors.fg_dark = fg_dark
-                 colors.fg_float = fg
-                 colors.fg_gutter = fg_gutter
-                 colors.fg_sidebar = fg_dark
-               end,
-             })
-             -- load the colorscheme here
-             vim.cmd([[colorscheme tokyonight]])
-           end,
-         },
-	 -- {
-	 --         "samharju/synthweave.nvim",
-	 --         lazy = false, -- Load this during startup if it's your main colorscheme
-	 --         priority = 1000,
-	 --         config = function()
-	 --        	 vim.cmd.colorscheme("synthweave")
-	 --         end,
-	 -- },
-      },
-      checker = { enabled = true },
-})
-
-
 -- Basic Behavior
 vim.opt.number = true -- Show line numbers
 vim.opt.wrap = true -- Wrap lines
@@ -123,7 +17,7 @@ vim.opt.cursorline = true -- highlight the current cursor line
 -- Appearance
 vim.opt.termguicolors = true -- Enable true colors
 vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-
+vim.cmd("let g:netrw_liststyle = 3")
 
 -- Highlight unnecessary whitespace in C and C++ files
 vim.api.nvim_create_autocmd('FileType', {
@@ -140,7 +34,6 @@ vim.keymap.set('n', 'j', 'gj') -- Move vertically by visual line
 vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('i', 'jj', '<Esc>') -- Easy escape
 vim.g.mapleader = ' ' -- Use SPACE as leader key
-vim.keymap.set('n', '<leader>p', ':Telescope find_files<CR>') -- Easy fuzzy file search
 
 -- Settings for faster grepping
 if vim.fn.executable('rg') ~= 0 then
@@ -151,6 +44,18 @@ end
 
 -- Settings for yanking to/from system clipboard
 vim.keymap.set('n', '<leader>y', '"+y') -- Copy to system clipboard
+
+-- Auto install of lazyvim --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
 
 -- Read extra init.lua if available
 local extra_init_path = vim.fn.expand('~/.config/nvim/extra_init.lua')
